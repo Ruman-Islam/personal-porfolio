d9ba7894c3f32940b593fa5448b3a9f0632d9be6
Ruman-Islam <rumanislam48@gmail.com>
2021-05-05 23:02:51 +0600

    make it better

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './Project.css';
 import Navbar from '../Shared/Navbar/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFile } from '@fortawesome/free-solid-svg-icons';
+import { faFile, faExternalLinkSquareAlt } from '@fortawesome/free-solid-svg-icons';
 import cv from '../../images/Ruman islam.pdf';
 import cover1 from '../../images/Screenshot_2.png';
 import cover2 from '../../images/Screenshot_1.png';
 import cover3 from '../../images/Screenshot_3.png';
+import {
+    faGithub,
+    faExter
+} from '@fortawesome/free-brands-svg-icons';
 
 const Projects = () => {
     return (
@@ -20,7 +24,7 @@ const Projects = () => {
                         <h1>Portfolio</h1>
                         <small>Welcome to my online portfolio. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. I'm taking on freelance work at the moment. Want some help building your software?</small>
                         <div className="resume">
-                            <button className="btn btn-secondary"><FontAwesomeIcon icon={faFile} /> <a style={{color: 'white', textDecoration: 'none'}} download href={cv}>Download Resume</a></button>
+                            <button className="btn btn-secondary"><FontAwesomeIcon icon={faFile} /> <a style={{ color: 'white', textDecoration: 'none' }} download href={cv}>Download Resume</a></button>
                         </div>
                     </div>
                 </div>
@@ -32,7 +36,14 @@ const Projects = () => {
                         </div>
                         <div style={{ padding: '10px' }}>
                             <h5>Medicine Corner</h5>
-                            <small>It's a single page web application where users can book car repair services with payment. An admin can update order status, add and remove services, and make another person admin.</small>
+                            <small>
+                                It is a online medicine & treatment store This is a react application with used React Router, firebase based hosting site and Node.js This is a ride share and searching website build by react js including
+                            </small>
+                        </div>
+                        <div style={{ padding: '10px' }}>
+                            <a style={{ marginRight: '20px' }} target="_blank" title="go to repository" href="https://github.com/Ruman-Islam/medicine-corner-client"><FontAwesomeIcon icon={faGithub} /> </a>
+
+                            <a target="_blank" title="go to live website" href="https://medicine-corner-824c6.web.app/"><FontAwesomeIcon icon={faExternalLinkSquareAlt} /> </a>
                         </div>
                     </div>
                     <div className="col-md-3 project">
@@ -41,7 +52,12 @@ const Projects = () => {
                         </div>
                         <div style={{ padding: '10px' }}>
                             <h5>BoiBoiBoi</h5>
-                            <small>It's a single page web application where users can book car repair services with payment. An admin can update order status, add and remove services, and make another person admin.</small>
+                            <small>It is a book shop This is a react application with used React Router, firebase based hosting site and Node.js This is a ride share and searching website build by react js including.</small>
+                        </div>
+                        <div style={{ padding: '10px' }}>
+                            <a style={{ marginRight: '20px' }} target="_blank" title="go to repository" href="https://boiboiboi.web.app/)"><FontAwesomeIcon icon={faGithub} /> </a>
+
+                            <a target="_blank" title="go to live website" href="https://github.com/Porgramming-Hero-web-course/full-stack-client-Ruman-Islam"><FontAwesomeIcon icon={faExternalLinkSquareAlt} /> </a>
                         </div>
                     </div>
                     <div className="col-md-3 project">
@@ -50,14 +66,19 @@ const Projects = () => {
                         </div>
                         <div style={{ padding: '10px' }}>
                             <h5>Rider Mama</h5>
-                            <small>It's a single page web application where users can book car repair services with payment. An admin can update order status, add and remove services, and make another person admin.</small>
+                            <small>This is a single page application with used React Router, firebase based hosting site. This is a ride share and searching website build by react js including.</small>
+                        </div>
+                        <div style={{ padding: '10px' }}>
+                            <a style={{ marginRight: '20px' }} target="_blank" title="go to repository" href="https://github.com/Porgramming-Hero-web-course/react-auth-Ruman-Islam"><FontAwesomeIcon icon={faGithub} /> </a>
+
+                            <a target="_blank" title="go to live website" href="https://rider-mama.web.app/"><FontAwesomeIcon icon={faExternalLinkSquareAlt} /> </a>
                         </div>
                     </div>
                 </div>
                 <div className="row mt-5 text-center">
                     <div className="col-md-12">
                         <small>Design & Build by © Ruman Islam</small>
-                        <br/><br/><br/><br/>
+                        <br /><br /><br /><br />
                     </div>
                 </div>
             </div>
